ae8100547ec1b5bb7c0fe2ce026e2615514a7a0d
seiless <tks00094@gmail.com>
2026-04-30 20:23:58 +0900

    확인사항 추가, 전처리 코드 베이스 추가

---
+++ 木材含水率予測/program/preprocess.py
@@ -0,0 +1,3 @@
+from read_data import load_datasets
+
+train_df, test_df = load_datasets()
--- 木材含水率予測/program/read_data.py
+++ 木材含水率予測/program/read_data.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "..", "data")
 
 
 def load_datasets():
-    """train.csv와 test.csv를 읽어 반환하는 함수"""
+    """trainデータとtestデータを読み込む関数"""
     train_path = os.path.join(DATA_DIR, "train.csv")
     test_path = os.path.join(DATA_DIR, "test.csv")
 
-    train_data = pd.read_csv(train_path, encoding="sjis")
-    test_data = pd.read_csv(test_path, encoding="sjis")
+    try:
 
-    print("데이터 로드 완료!")
-    return train_data, test_data
+        train_data = pd.read_csv(train_path, encoding="sjis")
+        test_data = pd.read_csv(test_path, encoding="sjis")
+
+        logger.info("データ読み込み成功")
+        return train_data, test_data
+    except Exception as e:
+        logger.exception(e)
